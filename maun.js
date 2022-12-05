0f231de9ff410a21143757b61df19b6b20b2592c
aigerimAlashova <aigerimalasheva39@gmail.com>
2022-12-05 16:48:40 +0600

    dzjs1

---
+++ maun.js
@@ -0,0 +1,34 @@
+// const username = prompt("Ведите имя")
+// const lastname = prompt("Ведите фамилию")
+// alert("Здраствуйте" + username)
+
+
+// const arr1 = Number(prompt("веди первое число"))
+// const arr2 = Number(prompt("веди второе число"))
+
+// if(arr1<arr2) {
+//     alert(arr2 + "больше" + arr1)
+// }else if(arr1 === arr2) {
+//     alert("Числа равны")
+// }else if (arr1 > arr2) {
+//     alert(arr1 + "больше"+ arr2)
+// }
+
+
+
+let color = prompt('Введите цвет');
+
+switch (color) {
+    case 'red':
+        alert('stop');
+        break;
+        case 'yellow':
+        alert('wait');
+        break;
+        case 'green':
+        alert('go');
+        breake;
+    default:
+        alert('unknow alret')
+        
+}
